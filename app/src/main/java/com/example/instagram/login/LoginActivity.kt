995c62fdbc2acf4bcb216d887421d8750795f7fd
joeun-01 <jjy020121@gmail.com>
2022-06-01 17:55:14 +0900

    바텀 네비게이션 수정

---
+++ app/src/main/java/com/example/instagram/login/LoginActivity.kt
@@ -0,0 +1,9 @@
+package com.example.instagram.login
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instagram.databinding.ActivityLoginBinding
+
+class LoginActivity: AppCompatActivity() {
+    lateinit var binding : ActivityLoginBinding
+
+}
