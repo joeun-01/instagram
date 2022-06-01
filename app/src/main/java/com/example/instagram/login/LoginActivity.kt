f296b3e7665293762e821b389425e352ad0b34ad
joeun-01 <jjy020121@gmail.com>
2022-06-01 20:10:36 +0900

    login, splash activity

--- app/src/main/java/com/example/instagram/login/LoginActivity.kt
+++ app/src/main/java/com/example/instagram/login/LoginActivity.kt
@@ -1,9 +1,18 @@
 package com.example.instagram.login
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.instagram.databinding.ActivityLoginBinding
 
 class LoginActivity: AppCompatActivity() {
     lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+    }
 
 }
