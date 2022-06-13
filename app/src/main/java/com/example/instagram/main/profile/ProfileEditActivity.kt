ce022c9b3f3b41f7cfc44663522e88206db5d787
dobby <dobbidd0@gmail.com>
2022-06-13 18:57:08 +0900

    profile getMyIdx()

--- app/src/main/java/com/example/instagram/main/profile/ProfileEditActivity.kt
+++ app/src/main/java/com/example/instagram/main/profile/ProfileEditActivity.kt
@@ -1,13 +1,16 @@
 package com.example.instagram.main.profile
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.instagram.databinding.ActivityProfileEditBinding
 import com.example.instagram.main.MainActivity
+import com.example.instagram.room.InstagramDatabase
 
 class ProfileEditActivity : AppCompatActivity() {
     private lateinit var binding : ActivityProfileEditBinding
+    private lateinit var instaDB : InstagramDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +18,19 @@ class ProfileEditActivity : AppCompatActivity() {
         binding = ActivityProfileEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        instaDB = InstagramDatabase.getInstance(this)!!
+
         binding.editCancelTv.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
+        //binding.editNameEt.text= instaDB.userDao().getUserID(getMyIdx())
+
+    }
+
+    private fun getMyIdx(): Int {  // 내 정보를 가져오기 위한 함수
+        val userSP = this.getSharedPreferences("user", MODE_PRIVATE)
+
+        return userSP.getInt("userIdx", 0)
     }
 }
--- app/src/main/java/com/example/instagram/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/instagram/main/profile/ProfileFragment.kt
@@ -1,19 +1,23 @@
 package com.example.instagram.main.profile
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.instagram.R
 import com.example.instagram.databinding.FragmentProfileBinding
+import com.example.instagram.room.InstagramDatabase
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.tabs.TabLayoutMediator
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding : FragmentProfileBinding
+    private lateinit var instaDB : InstagramDatabase
 
     private var tab = arrayListOf(R.drawable.ic_grid, R.drawable.ic_tag)
 
@@ -24,6 +28,10 @@ class ProfileFragment : Fragment() {
     ): View? {
 
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+        instaDB = InstagramDatabase.getInstance(requireContext())!!
+
+        // 로그인한 사용자 ID 연동
+        binding.profileNameTv.text = instaDB.userDao().getUserID(getMyIdx())
 
         // ViewPager 연결
         val profileVPAdapter = ProfileVPAdapter(this)
@@ -82,8 +90,13 @@ class ProfileFragment : Fragment() {
 //            dialog.dismiss()  // dialog 종료
 //        }
 
-
         return binding.root
+    }
+
+    private fun getMyIdx(): Int {  // 내 정보를 가져오기 위한 함수
+        val userSP = requireActivity().getSharedPreferences("user", Context.MODE_PRIVATE)
+
+        return userSP.getInt("userIdx", 0)
     }
 
     private fun setTextStatus(isVisible: Boolean){
