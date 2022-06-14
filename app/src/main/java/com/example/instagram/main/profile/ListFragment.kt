59bd0ec35a6fc8b3dbbb4c97cc6d1d9fad3ecd6d
dobby <dobbidd0@gmail.com>
2022-06-14 19:52:03 +0900

    settings fragment

---
+++ app/src/main/java/com/example/instagram/main/profile/ListFragment.kt
@@ -0,0 +1,28 @@
+package com.example.instagram.main.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.instagram.databinding.FragmentListBinding
+
+class ListFragment: Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.listSettingTv.setOnClickListener {
+            // SettingsFragment 로 전환!
+        }
+
+        return binding.root
+    }
+
+}
