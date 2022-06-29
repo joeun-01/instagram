1d91c6b008748109d2361c534fb8382eac48a99d
SUBIN PARK <psb8909@naver.com>
2022-06-30 02:29:59 +0900

    서치 게시물 파이어베이스 연결

---
+++ app/src/main/java/com/example/instagram/data/SearchDB.kt
@@ -0,0 +1,9 @@
+package com.example.instagram.data
+
+data class SearchDB (
+    // Search fragment 정보 저장
+    var uid : String? = "",  // 게시물을 올린 사람
+    var postIdx : Int = 0,  // 게시물 구분
+    var picture : Int = 0,  // 게시물 사진, 일단 한 개
+    var text : String = "",  // 게시물 내용
+)
