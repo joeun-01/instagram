00705e4fa60edb85c31ef76a6116c432318f87c9
dobby <dobbidd0@gmail.com>
2022-06-30 19:40:24 +0900

    다시 커밋 시도

--- app/src/main/java/com/example/instagram/main/profile/GridPostFragment.kt
+++ app/src/main/java/com/example/instagram/main/profile/GridPostFragment.kt
@@ -1,4 +1,12 @@
 package com.example.instagram.main.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.instagram.databinding.FragmentGridPostBinding
+
 //
 //import android.os.Bundle
 //import android.view.LayoutInflater
@@ -12,18 +20,18 @@ package com.example.instagram.main.profile
 //import com.example.instagram.room.InstagramDatabase
 //import java.util.ArrayList
 //
-//class GridPostFragment : Fragment() {
-//    lateinit var binding : FragmentGridPostBinding
+class GridPostFragment : Fragment() {
+    lateinit var binding : FragmentGridPostBinding
 //
 //    private lateinit var instaDB : InstagramDatabase
 //    private var post = ArrayList<Post>()
 //
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        binding = FragmentGridPostBinding.inflate(inflater, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentGridPostBinding.inflate(inflater, container, false)
 //
 //        instaDB = InstagramDatabase.getInstance(requireContext())!!
 //
@@ -33,6 +41,6 @@ package com.example.instagram.main.profile
 //        binding.profilePostGridRv.adapter = gridPostRVAdapter
 //        binding.profilePostGridRv.layoutManager = GridLayoutManager(context, 3)
 //
-//        return binding.root
-//    }
-//}
+       return binding.root
+    }
+}
