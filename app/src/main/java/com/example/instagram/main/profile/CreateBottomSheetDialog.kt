6e2e34a406853c3a4f0a32acd0fd5a4e376def0a
dobby <dobbidd0@gmail.com>
2022-06-30 22:45:07 +0900

    add post

--- app/src/main/java/com/example/instagram/main/profile/CreateBottomSheetDialog.kt
+++ app/src/main/java/com/example/instagram/main/profile/CreateBottomSheetDialog.kt
@@ -1,5 +1,6 @@
 package com.example.instagram.main.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import com.example.instagram.R
 import com.example.instagram.databinding.FragmentCreateBinding
 import com.example.instagram.databinding.FragmentListBinding
 import com.example.instagram.main.MainActivity
+import com.example.instagram.main.home.AddPostActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class CreateBottomSheetDialog: BottomSheetDialogFragment() {
@@ -20,6 +22,10 @@ class CreateBottomSheetDialog: BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCreateBinding.inflate(inflater, container, false)
+
+        binding.createPostTv.setOnClickListener {
+            startActivity(Intent(activity, AddPostActivity::class.java))
+        }
 
         return binding.root
     }
